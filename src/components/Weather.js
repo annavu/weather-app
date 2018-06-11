81f003d1e6b4c01c598f8d47acfcc5646517ce20
annavu <anna.wrzos@gmail.com>
2018-06-11 19:37:20 +0200

    add className and css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,12 +5,12 @@ class Weather extends React.Component {
     return (
       <div className="weather">
         {this.props.icon && <img src={this.props.icon} alt=""/>}
-        {this.props.city && <p>{this.props.city}, {this.props.country}</p> }
+        {this.props.city && <p className="weather__city">{this.props.city}, {this.props.country}</p> }
         {this.props.temperature && <p>Tempareture: {this.props.temperature}</p> }
         {this.props.humidity && <p>Humidity:  {this.props.humidity}</p> }
         {this.props.description && <p>Conditions: {this.props.description}</p> }
         {this.props.sunrise && <p>Sunrise/Sunset: {this.props.sunrise}/{this.props.sunset} (Central European Time)</p> }
-        {this.props.error && <p>{this.props.error}</p> }
+        {this.props.error && <p className="weather__alert">{this.props.error}</p> }
       </div>
     );
   }
